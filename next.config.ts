5ef36305fedb47241d34c5adfa3d6751c8785b67
unknown <trantoanthang2005@gmail.com>
2026-07-16 23:24:11 +0300

    basepath change

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/DuAnNoiDoHN",
+  basePath: basePath || undefined,
 };
 
 export default nextConfig;
